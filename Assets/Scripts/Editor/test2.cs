43f054694ffd782d5ffffc1e07eca98226daf37a
hanlinhe <hanlin.he@igg>
2021-01-29 18:02:24 +0800

    1

---
+++ Assets/Scripts/Editor/test2.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.IO;
+
+
+public class BuildAssetBundles2
+{
+    [MenuItem("test/BuildBundles")]
+    public static void Builde()
+    {
+        
+        AssetImporter.GetAtPath("Assets/GameRes/UI/Common_fui.bytes").assetBundleName = "hh.ab";
+        AssetImporter.GetAtPath("Assets/GameRes/UI/Login_fui.bytes").assetBundleName = "hh.ab";
+        BuildPipeline.BuildAssetBundles(Application.streamingAssetsPath, BuildAssetBundleOptions.None, BuildTarget.Android);
+    }
+
+}
--- Assets/Scripts/test.cs
+++ Assets/Scripts/test.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using FairyGUI;
 using GameFramework.FGUI;
 using LrsJudgeTools.Login;
@@ -10,10 +11,25 @@ public class test : MonoBehaviour
 {
     private void Awake()
     {
-        //Resources.Load("Assets/Resources/GameRes/UI/Common_fui.bytes");
-        //Resources.Load("Assets/Resources/GameRes/UI/Login_fui.bytes");
-        UIPackage.AddPackage("GameRes/UI/Login");
+        //以下是成功的
+        // assetbundle方式
+        string str = Path.Combine(Application.streamingAssetsPath, "hh.ab");
+        
+        str =
+#if UNITY_ANDROID
+            "jar:file://"+Application.dataPath+"!/assets/";
+#elif UNITY_IPHONE
+            Application.dataPath+"/Raw/";
+#elif UNITY_STANDALONE_WIN||UNITY_EDITOR
+           "file://" + Application.dataPath + "/StreamingAssets/";
+#endif
+        
+        AssetBundle bundle = AssetBundle.LoadFromFile(str);
+        UIPackage.AddPackage(bundle);
+        // Resouce加载方式
         //UIPackage.AddPackage("Assets/Resources/GameRes/UI/Login");
+        // 编辑器加载模式
+        //UIPackage.AddPackage("Assets/GameRes/UI/Login");
         LoginBinder.BindAll();
     }
 
@@ -21,6 +37,7 @@ public class test : MonoBehaviour
     void Start()
     {
         var ui = new BaseWindow();
+        Debug.Log("asdf");
         ui.Show();
     }
 
